2681b5cfe5170d9459305347fc41b8f5d9694389
Niels Visbeek <nielsvisbeek@outlook.com>
2023-08-15 20:04:47 +0200

    Update boilerplate with commands extended interface example.

--- Features/Vehicles/Commands/SpawnVehicleCommand.cs
+++ Features/Vehicles/Commands/SpawnVehicleCommand.cs
@@ -1,3 +1,4 @@
+using AltV.Icarus.Boilerplate.Features.Commands.Extensions;
 using AltV.Icarus.Commands.Interfaces;
 using AltV.Net;
 using AltV.Net.Async;
@@ -5,12 +6,13 @@ using AltV.Net.Elements.Entities;
 
 namespace AltV.Icarus.Boilerplate.Features.Vehicles.Commands;
 
-public class SpawnVehicleCommand : ICommand
+public class SpawnVehicleCommand : IExtendedCommand
 {
     public string Name { get; set; } = "vehicle";
     public string[ ]? Aliases { get; set; } = new[ ] { "v", "sv" };
-    public string Description { get; set; } = "/vehicle [model name]";
+    public string Description { get; set; } = "Spawns the specified vehicle at your location";
     public uint RequiredLevel { get; set; } = 0;
+    public string HelpText { get; set; } = "/vehicle [model name]";
     
     public async Task OnCommand( IPlayer player, string vehicleName )
     { 
@@ -23,7 +25,7 @@ public class SpawnVehicleCommand : ICommand
         
         if( model == default )
         {
-            player.Emit( "chat:message", $"[Usage] { Description }" );
+            player.Emit( "chat:message", $"[Usage] { HelpText }" );
             //TODO: Send chat message once chat module is added
             return;
         }
@@ -31,4 +33,5 @@ public class SpawnVehicleCommand : ICommand
         var vehicle = await AltAsync.CreateVehicle( model, player.Position, player.Rotation );
         player.SetIntoVehicle( vehicle, 1 );
     }
+
 }
