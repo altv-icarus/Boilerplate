18c23fed7260bcd7fce2726830a9c8c0f85b1a23
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:28:19 +0100

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using AltV.Net.Async;
+
+namespace AltV.Icarus.Boilerplate;
+
+public class Startup : AsyncResource
+{
+    private Bootstrapper _bootstrapper;
+    
+    public override async void OnStart( )
+    {
+        _bootstrapper = new Bootstrapper( );
+        await _bootstrapper.RunAsync( );
+    }
+
+    public override void OnStop( )
+    {
+        throw new NotImplementedException( );
+    }
+}
