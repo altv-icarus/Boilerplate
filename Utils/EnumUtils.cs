5d63e56613ddf2e9e391e759ceb22c8ca918262b
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:28:20 +0100

    Remove utils proj reference. Move enum utils to boilerplate proj. Adjust command for package update

---
+++ Utils/EnumUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace AltV.Icarus.Boilerplate.Utils;
+
+
+public static class EnumUtils
+{
+    public static T? RandomEnumValue<T>( )
+    {
+        var v = Enum.GetValues( typeof( T ) );
+        return ( T ) v.GetValue( new Random( ).Next( v.Length ) )!;
+    }
+}
