6b1baa22e0e36a29cc848b115f13e3dd1a73955d
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:28:20 +0100

    Update boilerplate with commands extended interface example.

---
+++ Features/Commands/Extensions/IExtendedCommand.cs
@@ -0,0 +1,8 @@
+﻿using AltV.Icarus.Commands.Interfaces;
+
+namespace AltV.Icarus.Boilerplate.Features.Commands.Extensions;
+
+public interface IExtendedCommand : ICommand
+{
+    string HelpText { get; set; }
+}
