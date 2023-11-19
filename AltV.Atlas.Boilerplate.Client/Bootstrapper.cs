a3651cffc677f123a723623d5adb1c5ef3f3a9da
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-19 09:56:34 +0100

    Move to new folder. Add client-side boilerplate project

---
+++ AltV.Atlas.Boilerplate.Client/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using AltV.Atlas.Client.DependencyInjection;
+using AltV.Atlas.Peds.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AltV.Atlas.Boilerplate.Client;
+
+public class Bootstrapper
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IServiceCollection _serviceCollection;
+    private readonly Logger<Bootstrapper> _logger;
+
+    public Bootstrapper( )
+    {
+        Console.WriteLine( "Initiating bootstrapper" );
+
+        _serviceCollection = new ServiceCollection( );
+        ConfigureServices( );
+        
+        _serviceProvider = _serviceCollection.BuildServiceProvider( );
+        _logger = _serviceProvider.GetService<Logger<Bootstrapper>>( )!;
+    }
+
+    public IServiceProvider Services => _serviceProvider;
+    
+    private void ConfigureServices( )
+    {
+        _serviceCollection.AddTransient<Logger<Bootstrapper>>( );
+        _serviceCollection.AddSingleton( sp => sp );
+
+        _serviceCollection.RegisterPedModule( );
+    }
+
+    public void Run( )
+    {
+        _serviceProvider.ResolveStartupServices( );
+        _serviceProvider.InitializePedModule( );
+        
+        _logger.LogInformation( "Bootstrapper initiated" );
+        Console.WriteLine( "" );
+        Console.WriteLine( "|------------------------------------------------------------------------------|" );
+        Console.WriteLine( "|               alt:V MP Atlas Client-side boilerplate started!                |" );
+        Console.WriteLine( "|------------------------------------------------------------------------------|" );
+        Console.WriteLine( $"| .NET Version: {Environment.Version.ToString( )}                                                          |" );
+        Console.WriteLine( "|------------------------------------------------------------------------------|" );
+        Console.WriteLine( "" );
+    }
+}
---
+++ AltV.Atlas.Boilerplate.Client/Startup.cs
@@ -0,0 +1,33 @@
+﻿using AltV.Net.Client;
+using AltV.Net.Client.Async;
+using AltV.Net.Client.Elements.Factories;
+using AltV.Net.Client.Elements.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AltV.Atlas.Boilerplate.Client;
+
+public class Startup : AsyncResource
+{
+    private Bootstrapper _bootstrapper;
+
+    public Startup()
+    {
+        _bootstrapper = new Bootstrapper(  );
+    }
+
+    public override void OnStart( )
+    {
+        _bootstrapper.Run( );
+    }
+
+    public override void OnStop( )
+    {
+        throw new NotImplementedException( );
+    }
+    
+    public override IEntityFactory<IPed> GetPedFactory( )
+    {
+        Alt.Log( "GetPedFactory" );
+        return _bootstrapper.Services.GetService<IEntityFactory<IPed>>( )!;
+    }
+}
